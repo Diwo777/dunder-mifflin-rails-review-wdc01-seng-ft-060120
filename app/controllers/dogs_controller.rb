d0a09be7a79a3946aa428df90a8dfc5caca271a4
Catrina Friday <cat.friday@gmail.com>
2020-06-24 17:44:43 -0400

    finished the whole lab

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,11 +1,11 @@
 class DogsController < ApplicationController
     def index
         @dogs = Dog.all
+        #byebug
     end
     
     def show
         id = params[:id]
         @dog = Dog.find(id) 
-        render :show
     end 
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,17 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        id = params[:id]
+        @employee = Employee.find(id)  
+    end
+end
+
+private
+
+def to_s
+    "#{@employee.first_name}" + " " + "#{@employee.last_name}"
 end
